d47467486a2c61ec1f73eb696bd9c539f1ea0282
Stanislas Quastana <squastana@hotmail.com>
2019-10-29 15:32:44 +0100

    ajout commande az account list-locations

--- var-aks-cluster.tf
+++ var-aks-cluster.tf
@@ -8,6 +8,7 @@ variable "keyvault_name" {
 
 variable "azure_region" {
   description = "Azure Region where to deploy resources. Caution the region must support Availability Zone"
+  # To get names of Azure Region : az account list-locations
   # To check support of Availability Zone in the Azure Region see https://docs.microsoft.com/bs-latn-ba/azure/availability-zones/az-overview
   type    = string
   default = "westeurope"
